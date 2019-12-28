61b7768859d89b6a1d30f79a2f1893bba1e78286
thenjip <39303840+thenjip@users.noreply.github.com>
2019-12-28 16:02:20 +0100

    Run all the modules containing a main.

--- src/nim_iterator_stream_experiment.nim
+++ src/nim_iterator_stream_experiment.nim
@@ -4,3 +4,27 @@ import nim_iterator_stream_experiment/[identity, optional, stream, unit, utils]
 
 export identity, optional, stream, unit, utils
 
+
+
+when isMainModule:
+  import std/[os, strformat, sequtils, strutils]
+
+
+
+  func moduleDir (): string =
+    let (dir, file, _) = currentSourcePath().splitFile()
+
+    dir / file
+
+
+  proc exec (cmdLine: string) =
+    if cmdLine.execShellCmd() != QuitSuccess:
+      osLastError().raiseOSError("Command failed:\n" & cmdLine)
+
+
+
+  let cmdLine = @[getCurrentCompilerExe(), $'c', "-r",]
+
+  for kind, path in moduleDir().walkDir(false):
+    if kind == pcFile and path.endsWith(fmt"{ExtSep}nim"):
+      cmdLine.`&`(path).foldl(a & " " & b.quoteShell(), "").exec()
