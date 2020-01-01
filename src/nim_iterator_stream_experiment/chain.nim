d503199ca5aa688c82294524ffc0dd4f61238325
thenjip <39303840+thenjip@users.noreply.github.com>
2020-01-01 17:27:22 +0100

    Add composition functions.

---
+++ src/nim_iterator_stream_experiment/chain.nim
@@ -0,0 +1,21 @@
+import std/[sugar]
+
+
+
+func chain* [A; B; C](f: A -> B; g: B -> C): A -> C =
+  (a: A) => a.f().g()
+
+
+
+func chain* [A; B](f: () -> A; g: A -> B): () -> B =
+  () => f().g()
+
+
+
+when isMainModule:
+  import std/[os, unittest]
+
+
+
+  suite currentSourcePath().splitFile().name:
+    discard
