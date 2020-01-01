2b80bff236091af30b82077a02c026db97983a41
thenjip <39303840+thenjip@users.noreply.github.com>
2020-01-01 17:27:51 +0100

    Use composition functions in 'optional' module.

--- src/nim_iterator_stream_experiment/optional.nim
+++ src/nim_iterator_stream_experiment/optional.nim
@@ -1,16 +1,30 @@
-import utils
+import chain, identity, utils
 
 import std/[sugar]
 
 
 
-type Optional* [T] = object
-  case empty: bool
-    of true:
-      discard
-    of false:
-      value: T
+{.experimental: "notnil".}
 
+
+
+type
+  SomePointer* = ref or ptr or pointer or cstring or proc
+
+  Optional* [T] = object
+    when T is SomePointer:
+      value: T
+    else:
+      case empty: bool
+        of true:
+          discard
+        of false:
+          value: T
+
+
+
+func toNone* (T: typedesc[SomePointer]): Optional[T] =
+  Optional[T](value: nil)
 
 
 func toNone* (T: typedesc): Optional[T] =
@@ -21,11 +35,20 @@ func toNone* [T](): Optional[T] =
   T.toNone()
 
 
-proc toSome* [T](value: T): Optional[T] =
+func toSome* [T: SomePointer](value: T): Optional[T] =
+  Optional[T](value: value)
+
+
+func toSome* [T: not SomePointer](value: T): Optional[T] =
   Optional[T](empty: false, value: value)
 
 
-func isNone* [T](self: Optional[T]): bool =
+
+func isNone* [T: SomePointer](self: Optional[T]): bool =
+  self.value.isNil()
+
+
+func isNone* [T: not SomePointer](self: Optional[T]): bool =
   self.empty
 
 
@@ -33,11 +56,13 @@ func isSome* [T](self: Optional[T]): bool =
   not self.isNone()
 
 
+
+proc ifNone* [A; B](self: Optional[A]; then: () -> B; `else`: A -> B): B =
+  self.isNone().ifElse(then, () => self.value.`else`())
+
+
 proc ifSome* [A; B](self: Optional[A]; then: A -> B; `else`: () -> B): B =
-  self.isSome().ifElse(
-    () => self.value.then(),
-    `else`
-  )
+  self.isSome().ifElse(() => self.value.then(), `else`)
 
 
 proc flatMap* [A; B](self: Optional[A]; f: A -> Optional[B]): Optional[B] =
@@ -45,11 +70,11 @@ proc flatMap* [A; B](self: Optional[A]; f: A -> Optional[B]): Optional[B] =
 
 
 proc map* [A; B](self: Optional[A]; f: A -> B): Optional[B] =
-  self.flatMap((value: A) => value.f().toSome())
+  self.flatMap(f.chain(toSome))
 
 
-proc get* [T](self: Optional[T]): T {.raises: [Exception, ValueError].} =
-  self.ifSome(value => value, proc (): T = raise ValueError.newException(""))
+func get* [T](self: Optional[T]): T {.raises: [Exception, ValueError].} =
+  self.ifSome(itself, proc (): T = raise ValueError.newException(""))
 
 
 proc filter* [T](self: Optional[T]; predicate: T -> bool): Optional[T] =
@@ -58,7 +83,8 @@ proc filter* [T](self: Optional[T]; predicate: T -> bool): Optional[T] =
   )
 
 
-proc `==`* [T](self, other: Optional[T]): bool =
+
+func `==`* [T](self, other: Optional[T]): bool =
   self.ifSome(
     selfValue =>
       other.ifSome(otherValue => selfValue == otherValue, () => false)
@@ -69,9 +95,288 @@ proc `==`* [T](self, other: Optional[T]): bool =
 
 
 when isMainModule:
+  import unit
+
   import std/[os, unittest]
 
 
 
+  template lazyCheck (checks: untyped): proc (): Unit =
+    (
+      proc (): Unit =
+        check:
+          checks
+    )
+
+
+
   suite currentSourcePath().splitFile().name:
-    discard
+    test "toSome: not SomePointer":
+      proc toSomeNotSomePtrTest [T: not SomePointer](expectedVal: T) =
+        let
+          expected = Optional[T](empty: false, value: expectedVal)
+          sut = expectedVal.toSome()
+
+        check:
+          sut == expected
+
+
+      toSomeNotSomePtrTest(1)
+      toSomeNotSomePtrTest(@[0])
+      toSomeNotSomePtrTest("a")
+
+
+
+    test "toSome: SomePointer":
+      proc toSomeSomePtrTest [T: SomePointer](expectedVal: T) =
+        let
+          expected = Optional[T](value: expectedVal)
+          sut = expectedVal.toSome()
+
+        check:
+          sut == expected
+
+
+      toSomeSomePtrTest("a".cstring)
+      toSomeSomePtrTest(() => -1)
+      toSomeSomePtrTest(ArithmeticError.newException(""))
+
+
+
+    test "toNone: not SomePointer":
+      proc toNoneNotSomePtrTest (T: typedesc[not SomePointer]) =
+        let
+          expected = Optional[T](empty: true)
+          sut = T.toNone()
+
+        check:
+          sut == expected
+
+
+      toNoneNotSomePtrTest(set[char])
+      toNoneNotSomePtrTest({'a': 0}.typeof())
+
+
+
+    test "toNone: SomePointer":
+      proc toNoneSomePtrTest (T: typedesc[SomePointer]) =
+        let
+          expected = Optional[T](value: nil)
+          sut = T.toNone()
+
+        check:
+          sut == expected
+
+
+      toNoneSomePtrTest(pointer)
+      toNoneSomePtrTest(ref RootObj)
+      toNoneSomePtrTest(() -> void)
+
+
+
+    test "ifSome: with some":
+      proc ifSomeWithSomeTest [A; B](initialVal: A; expected, unexpected: B) =
+        require:
+          expected != unexpected
+
+        let sut =
+          initialVal
+          .toSome()
+          .ifSome(
+            a => lazyCheck(a == initialVal).chain(_ => expected)(),
+            () => unexpected
+          )
+
+        check:
+          sut == expected
+
+
+      ifSomeWithSomeTest(['a'], 0, 1)
+      ifSomeWithSomeTest("a", () => 0, () => 0)
+
+
+
+    test "ifSome: with none":
+      proc ifSomeWithNoneTest [B](A: typedesc; expected, unexpected: B) =
+        require:
+          expected != unexpected
+
+        let sut = A.toNone().ifSome(_ => unexpected, () => expected)
+
+        check:
+          sut == expected
+
+
+      ifSomeWithNoneTest(uint16, "a", "b")
+      ifSomeWithNoneTest(seq[int], false, true)
+
+
+
+    test "ifNone: with some":
+      proc ifNoneWithSomeTest [A; B](initialVal: A; expected, unexpected: B) =
+        require:
+          expected != unexpected
+
+        let sut =
+          initialVal
+          .toSome()
+          .ifNone(
+            () => unexpected,
+            a => lazyCheck(a == initialVal).chain(_ => expected)()
+          )
+
+        check:
+          sut == expected
+
+
+      ifNoneWithSomeTest("a", 'a', 'b')
+
+
+
+    test "ifNone: with none":
+      proc ifNoneWithNoneTest [B](A: typedesc; expected, unexpected: B) =
+        require:
+          expected != unexpected
+
+        let sut = A.toNone().ifNone(() => expected, _ => unexpected)
+
+        check:
+          sut == expected
+
+
+      ifNoneWithNoneTest(cfloat, -1, 6)
+
+
+
+    test "get: with some":
+      proc getWithSomeTest [T](expected: T) =
+        let sut = expected.toSome().get()
+
+        check:
+          sut == expected
+
+
+      getWithSomeTest(["a", "b"])
+
+
+
+    test "get: with none":
+      proc getWithNoneTest (T: typedesc) =
+        expect ValueError:
+          T.toNone().get().ignore()
+
+
+      getWithNoneTest(int)
+
+
+
+    test "flatMap: with some":
+      proc flatMapWithSomeTest [A; B](initialVal: A; expectedVal: B) =
+        let
+          expected = expectedVal.toSome()
+          sut =
+            initialVal
+            .toSome()
+            .flatMap(
+              (a: A) =>
+                lazyCheck(a == initialVal).chain(_ => expectedVal.toSome())()
+            )
+
+        check:
+          sut == expected
+
+
+      flatMapWithSomeTest(0, 'a')
+
+
+
+    test "flatMap: with none":
+      proc flatMapWithNoneTest [B](A: typedesc; unexpectedVal: B) =
+        let
+          expected = B.toNone()
+          sut = A.toNone().flatMap((_: A) => unexpectedVal.toSome())
+
+        check:
+          sut == expected
+
+
+      flatMapWithNoneTest(string, () => 0.0)
+
+
+
+    test "map: with some":
+      proc mapWithSomeTest [A; B](initialVal: A; expectedVal: B) =
+        let
+          expected = expectedVal.toSome()
+          sut =
+            initialVal
+            .toSome()
+            .map(a => lazyCheck(a == initialVal).chain(_ => expectedVal)())
+
+        check:
+          sut == expected
+
+
+      mapWithSomeTest(2.toBiggestFloat(), {0})
+
+
+
+    test "map: with none":
+      proc mapWithNoneTest [B](A: typedesc; unexpectedVal: B) =
+        let
+          expected = B.toNone()
+          sut = A.toNone().map(_ => unexpectedVal)
+
+        check:
+          sut == expected
+
+
+      mapWithNoneTest(float32, 'a')
+
+
+
+    test "filter: some -> some":
+      proc filterSomeSomeTest [T](val: T; predicate: T -> bool) =
+        require:
+          val.predicate()
+
+        let
+          expected = val.toSome()
+          sut = val.toSome().filter(predicate)
+
+        check:
+          sut == expected
+
+
+      filterSomeSomeTest(86, i => i > 80)
+
+
+
+    test "filter: some -> none":
+      proc filterSomeNoneTest [T](val: T; predicate: T -> bool) =
+        require:
+          not val.predicate()
+
+        let
+          expected = T.toNone()
+          sut = val.toSome().filter(predicate)
+
+        check:
+          sut == expected
+
+
+        filterSomeNoneTest("", s => s.len() > 0)
+
+
+
+    test "filter: none -> none":
+      proc filterNoneNoneTest (T: typedesc; predicate: T -> bool) =
+        let
+          expected = T.toNone()
+          sut = T.toNone().filter(predicate)
+
+        check:
+          sut == expected
+
+
+      filterNoneNoneTest(char, c => true)
