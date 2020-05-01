00a8012476b9062cdaf7c79f5066bcc7db762538
thenjip <39303840+thenjip@users.noreply.github.com>
2020-05-01 18:28:08 +0200

    utils/call:   - Make "call(() -> T)" a proc instead of a template.

--- src/nim_iterator_stream_experiment/utils/call.nim
+++ src/nim_iterator_stream_experiment/utils/call.nim
@@ -2,7 +2,7 @@ import std/[macros, sugar]
 
 
 
-template call* [T](p: () -> T): T =
+proc call* [T](p: () -> T): T =
   p()
 
 
@@ -21,39 +21,35 @@ when isMainModule:
 
   suite currentSourcePath().splitFile().name:
     test "call: 0 arguments":
-      proc doTest [T](expected: T) =
-        let sut = () => expected
-
+      proc doTest [T](sut: () -> T) =
         check:
-          sut.call() == expected
+          sut.call() == sut()
 
 
-      doTest(156)
-      doTest("a")
+      doTest(() => 156)
+      doTest(() => "a")
 
 
 
     test "call: 1 argument":
-      proc doTest [T](expected: T) =
-        let sut = (a: T) => a
-
+      proc doTest [A; B](sut: A -> B; arg: A) =
         check:
-          sut.call(expected) == expected
+          sut.call(arg) == sut(arg)
 
 
-      doTest('a')
-      doTest(["a", "b"])
+      doTest((c: char) => c.byte, 'a')
+      doTest((s: seq[string]) => s, @["a", "b"])
 
 
 
     test "call: 2 arguments":
-      proc doTest [A; B; C](sut: (A, B) -> C; arg1: A; arg2: B; expected: C) =
+      proc doTest [A; B; C](sut: (A, B) -> C; arg1: A; arg2: B) =
         check:
-          sut.call(arg1, arg2) == expected
+          sut.call(arg1, arg2) == sut(arg1, arg2)
 
 
       proc plus [I: SomeInteger](a, b: I): I =
         a + b
 
 
-      doTest(plus[int], 1, 2, 1.plus(2))
+      doTest(plus[int], 1, 2)
