ba067d5de8d55536e4cc87ac584ecfb4fe0fa3e3
thenjip <39303840+thenjip@users.noreply.github.com>
2020-05-30 15:44:01 +0200

    - utils/ifelse:   - Add a test for "ifElse".

--- src/nim_iterator_stream_experiment/utils/ifelse.nim
+++ src/nim_iterator_stream_experiment/utils/ifelse.nim
@@ -2,7 +2,7 @@ import std/[sugar]
 
 
 
-proc ifElse* [T](condition: bool; then: () -> T; `else`: () -> T): T =
+proc ifElse* [T](condition: bool; then, `else`: () -> T): T =
   if condition:
     then()
   else:
@@ -11,9 +11,32 @@ proc ifElse* [T](condition: bool; then: () -> T; `else`: () -> T): T =
 
 
 when isMainModule:
+  import chain, ignore, operators, unit, variables
+  import ../monad/[identity]
+
   import std/[os, unittest]
 
 
 
   suite currentSourcePath().splitFile().name:
-    discard
+    test """"condition.ifElse(then, `else`)" should enter only 1 of the 2 paths.""":
+      proc doTest [T](condition: bool; then, `else`: () -> T) =
+        const expected = 1.Natural
+
+        var pathTaken = 0.Natural
+
+        proc incrementPathCounter [T](value: T): T =
+          pathTaken.modify(plus1).apply(_ => value)
+
+        condition
+          .ifElse(
+            then.chain(incrementPathCounter),
+            `else`.chain(incrementPathCounter)
+          ).ignore()
+
+        check:
+          pathTaken.read() == expected
+
+
+      doTest(false, () => unit(), () => unit())
+      doTest(true, () => -5, () => int.high())
