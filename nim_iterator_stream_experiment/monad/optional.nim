f0035bcb5bb5ea28c249ed5a9adfffa428b1037d
thenjip <39303840+thenjip@users.noreply.github.com>
2020-07-17 19:03:04 +0200

    - monad/optional:   - Add a permalink to the examples.

--- nim_iterator_stream_experiment/monad/optional.nim
+++ nim_iterator_stream_experiment/monad/optional.nim
@@ -6,6 +6,8 @@
   This is another implementation of the `Option`/`Maybe` monad, but using only
   pure functional programming techniques to make it compatible with compile time
   execution.
+
+  Examples can be found `here <https://github.com/thenjip/nim-iterator-stream-experiment/tree/5f1513037640708af8b59f2cf8e3fbca240de31b/examples/monad/optional>`_.
 ]##
 
 
@@ -159,238 +161,243 @@ when isMainModule:
 
 
 
-  suite currentSourcePath().splitFile().name:
-    test """The "Nilable" concept should match standard "nil" types.""":
-      proc doTest (T: typedesc) =
-        check:
-          T is Nilable
+  proc main () =
+    suite currentSourcePath().splitFile().name:
+      test """The "Nilable" concept should match standard "nil" types.""":
+        proc doTest (T: typedesc) =
+          check:
+            T is Nilable
 
 
-      doTest(ptr cstring)
-      doTest(pointer)
-      doTest(ref tuple[a: Unit; b: char])
-      doTest(int -> string)
-      doTest(cstring)
-
-
-
-    test """The "Nilable" concept should not match standard non "nil" types.""":
-      proc doTest (T: typedesc) =
-        check:
-          T isnot Nilable
-
-
-      doTest(cuint)
-      doTest(string)
-      doTest(seq[cdouble])
-      doTest(tuple[a: Unit; b: byte])
-      doTest(Slice[Natural])
-      doTest(Positive)
+        doTest(ptr cstring)
+        doTest(pointer)
+        doTest(ref tuple[a: Unit; b: char])
+        doTest(int -> string)
+        doTest(cstring)
 
 
 
-    test """"nil.toSome()" should raise a "Defect" at runtime.""":
-      proc doTest (T: typedesc[Nilable]) =
-        expect Defect:
-          nil.T.toSome().ignore()
+      test """The "Nilable" concept should not match standard non "nil" types.""":
+        proc doTest (T: typedesc) =
+          check:
+            T isnot Nilable
 
 
-      doTest(pointer)
-
-
-
-    test """"nil.toOptional()" should return "none".""":
-      proc doTest (T: typedesc[Nilable]) =
-        let
-          actual = nil.convert(T).toOptional()
-          expected = T.toNone()
-
-        check:
-          actual == expected
-
-
-      doTest(pointer)
-      doTest(Predicate[uint16])
-      doTest(ref Exception)
+        doTest(cuint)
+        doTest(string)
+        doTest(seq[cdouble])
+        doTest(tuple[a: Unit; b: byte])
+        doTest(Slice[Natural])
+        doTest(Positive)
 
 
 
-    test """"value.toOptional()" should return "some" when "value" is not "nil".""":
-      proc doTest [T: Nilable](value: T) =
-        let
-          actual = value.toOptional()
-          expected = value.toSome()
-
-        check:
-          actual == expected
+      test """"nil.toSome()" should raise a "Defect" at runtime.""":
+        proc doTest (T: typedesc[Nilable]) =
+          expect Defect:
+            nil.T.toSome().ignore()
 
 
-      doTest(doTest[cstring])
-      doTest(new byte)
+        doTest(pointer)
 
 
 
-    test """"Optional[T]" should obey the monad laws.""":
-      proc doTest [LA; LMA; LMB; RT; RM; AA; AB; AMA; AMB; AMC](
-        spec: MonadLawsSpec[LA, LMA, LMB, RT, RM, AA, AB, AMA, AMB, AMC]
-      ) =
-        check:
-          spec.checkMonadLaws()
+      test """"nil.toOptional()" should return "none".""":
+        proc doTest (T: typedesc[Nilable]) =
+          let
+            actual = nil.convert(T).toOptional()
+            expected = T.toNone()
+
+          check:
+            actual == expected
 
 
-      doTest(
-        monadLawsSpec(
-          leftIdentitySpec(NaN, toSome, _ => float32.toNone()),
-          ["".cstring, nil]
-            .apply(
-              expected =>
-                rightIdentitySpec(expected, _ => expected.typeof().toNone())
+        doTest(pointer)
+        doTest(Predicate[uint16])
+        doTest(ref Exception)
+
+
+
+      test """"value.toOptional()" should return "some" when "value" is not "nil".""":
+        proc doTest [T: Nilable](value: T) =
+          let
+            actual = value.toOptional()
+            expected = value.toSome()
+
+          check:
+            actual == expected
+
+
+        doTest(doTest[cstring])
+        doTest(new byte)
+
+
+
+      test """"Optional[T]" should obey the monad laws.""":
+        proc doTest [LA; LMA; LMB; RT; RM; AA; AB; AMA; AMB; AMC](
+          spec: MonadLawsSpec[LA, LMA, LMB, RT, RM, AA, AB, AMA, AMB, AMC]
+        ) =
+          check:
+            spec.checkMonadLaws()
+
+
+        doTest(
+          monadLawsSpec(
+            leftIdentitySpec(NaN, toSome, _ => float32.toNone()),
+            ["".cstring, nil]
+              .apply(
+                expected =>
+                  rightIdentitySpec(expected, _ => expected.typeof().toNone())
+              )
+            ,
+            associativitySpec(
+              69,
+              toSome,
+              i => toSome(toNone[i.typeof()]),
+              (p: toNone[int].typeof()) => p.call().`$`().toSome()
             )
-          ,
-          associativitySpec(
-            69,
-            toSome,
-            i => toSome(toNone[i.typeof()]),
-            (p: toNone[int].typeof()) => p.call().`$`().toSome()
           )
         )
-      )
 
 
 
-    test """Computations that use "Optional[T]" and without side effects should be compatible with compile time execution.""":
-      proc doTest [A; B](
-        sut: static[proc (argument: Optional[A]): B {.noSideEffect.}];
-        argument: static[proc (): Optional[A] {.noSideEffect.}];
-        expected: static B
-      ) =
-        const actual = sut.call(argument.call())
+      test """Computations that use "Optional[T]" and without side effects should be compatible with compile time execution.""":
+        proc doTest [A; B](
+          sut: static[proc (argument: Optional[A]): B {.noSideEffect.}];
+          argument: static[proc (): Optional[A] {.noSideEffect.}];
+          expected: static B
+        ) =
+          const actual = sut.call(argument.call())
 
-        check:
-          actual == expected
-
-
-
-      func sut1 [T](arg: Optional[T]): T =
-        arg.unbox()
-
-      func expected1 (): int =
-        -2
-
-      func argument1 (): Optional[expected1.returnType()] =
-        expected1().toSome()
-
-
-      func sut2 [T: Nilable](arg: Optional[T]): T =
-        arg.ifSome(itself, () => nil.T)
-
-      func expected2 (): ref char =
-        nil
-
-      func argument2 (): Optional[expected2.returnType()] =
-        expected2.returnType().toNone()
-
-
-      doTest(sut1[expected1.returnType()], argument1, expected1())
-      doTest(sut2[expected2.returnType()], argument2, expected2())
+          check:
+            actual == expected
 
 
 
-    test """"unboxOr" with "some" should return the value inside the "Optional".""":
-      proc doTest [T](expected: T; unexpected: T) =
-        require:
-          expected != unexpected
+        func sut1 [T](arg: Optional[T]): T =
+          arg.unbox()
 
-        let actual = expected.toSome().unboxOr(() => unexpected)
+        func expected1 (): int =
+          -2
 
-        check:
-          actual == expected
-
-
-      doTest("abc".cstring, nil)
-      doTest(0, 1)
+        func argument1 (): Optional[expected1.returnType()] =
+          expected1().toSome()
 
 
+        func sut2 [T: Nilable](arg: Optional[T]): T =
+          arg.ifSome(itself, () => nil.T)
 
-    test """"unboxOr" with "none" should return the default value passed.""":
-      proc doTest [T](expected: T) =
-        let actual = T.toNone().unboxOr(() => expected)
+        func expected2 (): ref char =
+          nil
 
-        check:
-          actual == expected
+        func argument2 (): Optional[expected2.returnType()] =
+          expected2.returnType().toNone()
 
 
-      doTest('a')
-      doTest(unboxOr[int32])
+        doTest(sut1[expected1.returnType()], argument1, expected1())
+        doTest(sut2[expected2.returnType()], argument2, expected2())
 
 
 
-    test """"unbox" with "some" should return the "expected" value.""":
-      proc doTest [T](expected: T) =
-        let actual = expected.toSome().unbox()
+      test """"unboxOr" with "some" should return the value inside the "Optional".""":
+        proc doTest [T](expected: T; unexpected: T) =
+          require:
+            expected != unexpected
 
-        check:
-          actual == expected
+          let actual = expected.toSome().unboxOr(() => unexpected)
 
-
-      doTest(["a", "b"])
-      doTest(1)
-      doTest((1, 1.005))
-      doTest(() => 5)
+          check:
+            actual == expected
 
 
-
-    test """"unbox" with "none" should raise an "UnboxError" at runtime.""":
-      proc doTest (T: typedesc) =
-        expect UnboxError:
-          T.toNone().unbox().ignore()
-
-
-      doTest(int)
+        doTest("abc".cstring, nil)
+        doTest(0, 1)
 
 
 
-    test """"self.filter(predicate)" with "none" should return "none".""":
-      proc doTest [T](predicate: Predicate[T]) =
-        let
-          expected = T.toNone()
-          actual = expected.filter(predicate)
+      test """"unboxOr" with "none" should return the default value passed.""":
+        proc doTest [T](expected: T) =
+          let actual = T.toNone().unboxOr(() => expected)
 
-        check:
-          actual == expected
+          check:
+            actual == expected
 
 
-      doTest((s: string) => s.len() > 0)
-      doTest(alwaysTrue[uint])
-      doTest(alwaysFalse[ptr cint])
+        doTest('a')
+        doTest(unboxOr[int32])
 
 
 
-    test """"self.filter(predicate)" with "some" and a predicate that will be verified should return "self".""":
-      proc doTest [T](value: T) =
-        let
-          expected = value.toSome()
-          actual = expected.filter(alwaysTrue[T])
+      test """"unbox" with "some" should return the "expected" value.""":
+        proc doTest [T](expected: T) =
+          let actual = expected.toSome().unbox()
 
-        check:
-          actual == expected
+          check:
+            actual == expected
 
 
-      doTest(new cfloat)
-      doTest('a')
+        doTest(["a", "b"])
+        doTest(1)
+        doTest((1, 1.005))
+        doTest(() => 5)
 
 
 
-    test """"self.filter(predicate)" with "some" and a predicate that will not be verified should return "none".""":
-      proc doTest [T](value: T) =
-        let
-          expected = T.toNone()
-          actual = value.toSome().filter(alwaysFalse[T])
-
-        check:
-          actual == expected
+      test """"unbox" with "none" should raise an "UnboxError" at runtime.""":
+        proc doTest (T: typedesc) =
+          expect UnboxError:
+            T.toNone().unbox().ignore()
 
 
-      doTest("abc".cstring)
-      doTest(partial($ ?:uint))
-      doTest(0)
+        doTest(int)
+
+
+
+      test """"self.filter(predicate)" with "none" should return "none".""":
+        proc doTest [T](predicate: Predicate[T]) =
+          let
+            expected = T.toNone()
+            actual = expected.filter(predicate)
+
+          check:
+            actual == expected
+
+
+        doTest((s: string) => s.len() > 0)
+        doTest(alwaysTrue[uint])
+        doTest(alwaysFalse[ptr cint])
+
+
+
+      test """"self.filter(predicate)" with "some" and a predicate that will be verified should return "self".""":
+        proc doTest [T](value: T) =
+          let
+            expected = value.toSome()
+            actual = expected.filter(alwaysTrue[T])
+
+          check:
+            actual == expected
+
+
+        doTest(new cfloat)
+        doTest('a')
+
+
+
+      test """"self.filter(predicate)" with "some" and a predicate that will not be verified should return "none".""":
+        proc doTest [T](value: T) =
+          let
+            expected = T.toNone()
+            actual = value.toSome().filter(alwaysFalse[T])
+
+          check:
+            actual == expected
+
+
+        doTest("abc".cstring)
+        doTest(partial($ ?:uint))
+        doTest(0)
+
+
+
+  main()
