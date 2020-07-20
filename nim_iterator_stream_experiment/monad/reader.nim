bcfca4d3d9a16190ead0a7d2f9c1e9785620ac8f
thenjip <39303840+thenjip@users.noreply.github.com>
2020-07-20 13:28:46 +0200

    - monad/reader:   - Put the test suite in a "main" proc.

--- nim_iterator_stream_experiment/monad/reader.nim
+++ nim_iterator_stream_experiment/monad/reader.nim
@@ -81,68 +81,73 @@ when isMainModule:
 
 
 
-  suite currentSourcePath().splitFile().name:
-    test """"Reader[S, T]" should obey the monad laws.""":
-      proc doTest [LA; LMA; LMB; LR; RT; RM; RR; AA; AB; AMA; AMB; AMC; AR](
-        spec: MonadLawsSpec[LA, LMA, LMB, LR, RT, RM, RR, AA, AB, AMA, AMB, AMC, AR]
-      ) =
-        check:
-          spec.checkMonadLaws()
+  proc main () =
+    suite currentSourcePath().splitFile().name:
+      test """"Reader[S, T]" should obey the monad laws.""":
+        proc doTest [LA; LMA; LMB; LR; RT; RM; RR; AA; AB; AMA; AMB; AMC; AR](
+          spec: MonadLawsSpec[LA, LMA, LMB, LR, RT, RM, RR, AA, AB, AMA, AMB, AMC, AR]
+        ) =
+          check:
+            spec.checkMonadLaws()
 
 
-      doTest(
-        monadLawsSpec(
-          leftIdentitySpec(
-            @["a", "abc", "012"],
-            partial(toReader(?_, Unit)),
-            s => s.foldl(a + b.len().uint, 0u).toReader(Unit),
-            unit()
-          ),
-          rightIdentitySpec({0..9}, partial(toReader(?_, cfloat)), 165.8),
-          associativitySpec(
-            ([0, 7], 'a'),
-            partial(toReader(?_, string)),
-            t => t[0].foldl(a + b, 0).plus(t[1].ord()).toReader(string),
-            (i: int) => i.modulo(2).toReader(string),
-            "0123"
+        doTest(
+          monadLawsSpec(
+            leftIdentitySpec(
+              @["a", "abc", "012"],
+              partial(toReader(?_, Unit)),
+              s => s.foldl(a + b.len().uint, 0u).toReader(Unit),
+              unit()
+            ),
+            rightIdentitySpec({0..9}, partial(toReader(?_, cfloat)), 165.8),
+            associativitySpec(
+              ([0, 7], 'a'),
+              partial(toReader(?_, string)),
+              t => t[0].foldl(a + b, 0).plus(t[1].ord()).toReader(string),
+              (i: int) => i.modulo(2).toReader(string),
+              "0123"
+            )
           )
         )
-      )
 
 
 
-    test """"Reader[S, T]" without side effects should be compatible with compile time execution.""":
-      template doTest [S; T](
-        sut: Reader[S, T]{noSideEffect};
-        state: static S;
-        expected: static T
-      ): proc () {.nimcall.} =
-        (
-          proc () =
-            const actual = sut.run(state)
+      test """"Reader[S, T]" without side effects should be compatible with compile time execution.""":
+        template doTest [S; T](
+          sut: Reader[S, T]{noSideEffect};
+          state: static S;
+          expected: static T
+        ): proc () =
+          (
+            proc () =
+              const actual = sut.run(state)
 
-            check:
-              actual == expected
-        )
-
-
-      func expected1 (): string =
-        "abc"
-
-      func sut1 (): Reader[Unit, expected1.returnType()] =
-        (_: Unit) => expected1.call()
+              check:
+                actual == expected
+          )
 
 
-      doTest(sut1.call(), unit(), expected1.call()).call()
+        func expected1 (): string =
+          "abc"
+
+        func sut1 (): Reader[Unit, expected1.returnType()] =
+          (_: Unit) => expected1.call()
+
+
+        doTest(sut1.call(), unit(), expected1.call()).call()
 
 
 
-    test """"S.ask()" should give access to the read state when using "flatMap".""":
-      proc doTest [S; T](self: Reader[S, T]; expected: S) =
-        let actual = self.flatMap((_: T) => S.ask()).run(expected)
+      test """"S.ask()" should give access to the read state when using "flatMap".""":
+        proc doTest [S; T](self: Reader[S, T]; expected: S) =
+          let actual = self.flatMap((_: T) => S.ask()).run(expected)
 
-        check:
-          actual == expected
+          check:
+            actual == expected
 
 
-      doTest(partial(len(?:string)), "abc")
+        doTest(partial(len(?:string)), "abc")
+
+
+
+  main()
