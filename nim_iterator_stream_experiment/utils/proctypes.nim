071d94936cdd60f02da12c6b51f6eabd5509cc1f
thenjip <39303840+thenjip@users.noreply.github.com>
2020-09-18 10:53:17 +0200

    utils/proctypes: Put the test suite in a main proc

--- nim_iterator_stream_experiment/utils/proctypes.nim
+++ nim_iterator_stream_experiment/utils/proctypes.nim
@@ -24,61 +24,63 @@ when isMainModule:
 
 
 
-  suite currentSourcePath().splitFile().name:
-    test """"T.returnType()" should return the return type of the given procedure type.""":
-      template doTest (
-        T: typedesc[proc];
-        expected: typedesc
-      ): proc () {.nimcall.} =
-        (
-          proc () =
-            check:
-              T.returnType().`is`(expected)
-        )
+  proc main () =
+    suite currentSourcePath().splitFile().name:
+      test """"T.returnType()" should return the return type of the given procedure type.""":
+        template doTest (T: typedesc[proc]; expected: typedesc): proc () =
+          (
+            proc () =
+              check:
+                T.returnType().`is`(expected)
+          )
 
 
-      type
-        SomeProc = ref NilAccessError -> seq[char]
-        SomeGenericProc [T; R] = T -> R
+        type
+          SomeProc = ref NilAccessError -> seq[char]
+          SomeGenericProc [T; R] = T -> R
 
 
-      for t in [
-        doTest(proc (), void),
-        doTest(
-          proc (a: string): Natural {.cdecl.},
-          range[Natural.low() .. Natural.high()]
-        ),
-        doTest((int, () -> tuple[]) -> pointer, pointer),
-        doTest(SomeProc, seq[char]),
-        doTest(
-          SomeGenericProc[float, ref FloatingPointError],
-          ref FloatingPointError
-        ),
-        doTest(() -> var int, var int)
-      ]:
-        t.call()
-
-
-
-    test """"p.returnType()" should return the return type of the given procedure instance.""":
-      template doTest (p: proc; expected: typedesc): proc () {.nimcall.} =
-        (
-          proc () =
-            check:
-              p.returnType().`is`(expected)
-        )
-
-
-      func someProc (): Positive {.closure.} =
-        1
-
-
-      when defined(js):
-        doTest(someProc, Positive).call()
-      else:
         for t in [
-          doTest(stackTraceAvailable, bool),
-          doTest(absolutePath, string),
-          doTest(someProc, Positive)
+          doTest(proc (), void),
+          doTest(
+            proc (a: string): Natural {.cdecl.},
+            range[Natural.low() .. Natural.high()]
+          ),
+          doTest((int, () -> tuple[]) -> pointer, pointer),
+          doTest(SomeProc, seq[char]),
+          doTest(
+            SomeGenericProc[float, ref FloatingPointError],
+            ref FloatingPointError
+          ),
+          doTest(() -> var int, var int)
         ]:
           t.call()
+
+
+
+      test """"p.returnType()" should return the return type of the given procedure instance.""":
+        template doTest (p: proc; expected: typedesc): proc () =
+          (
+            proc () =
+              check:
+                p.returnType().`is`(expected)
+          )
+
+
+        func someProc (): Positive {.closure.} =
+          1
+
+
+        when defined(js):
+          doTest(someProc, Positive).call()
+        else:
+          for t in [
+            doTest(stackTraceAvailable, bool),
+            doTest(absolutePath, string),
+            doTest(someProc, Positive)
+          ]:
+            t.call()
+
+
+
+  main()
